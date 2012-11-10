39cc2dbdc07271e778c27bdcdd6a13c713ffd24e
Terry <think@terry>
2012-11-10 15:26:26 -0700

    Completed section 6.6 (#the-model) of the Rails tutorial at http://guides.rubyonrails.org/getting_started.html

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '281e7c2b31a5b6706aa62be5f0a99252bbbb3a518e2f26b9fb316cab8be57f6c449ef5d70263f43e2c781aa9a08b83fb1c8b46e55df66ae35f0a2ef66ac43676'
