338c0a7d66666b63eb645ff4bb4d3711ef3e60d6
Terry <think@terry>
2012-12-13 16:49:35 -0700

    Created a new 'clip' Rails project in the 'ruby' directory to serve as the starting point for a consolidated Rails project (which is okay per http://stackoverflow.com/a/850185/770170).

---
+++ ruby/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clip::Application.config.secret_token = '272982ac1e1649205d23d6f267c8ff2eb79be9cf5dc31d3cccfe8cac144b738f99c9cf8f1d086a08e0e9e4898b558b3e97df06080510fab1898b8a86b6b1c767'
