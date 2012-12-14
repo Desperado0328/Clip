7dc985693fcf5220de127275190f9f1ab59d53a4
Terry <think@terry>
2012-12-13 17:41:14 -0700

    Deleted the corporate-ownership directory now that it's been merged into the main project.

--- ruby/corporate-ownership/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-CorporateOwnership::Application.config.secret_token = '2306cd54773f6b491bce243ffec09bbc8e87893d854ba1505d79ace142dce429f18d746013924eb73329129669b2f24f39ede494d88a3dc91f84943b206716c7'
