c502eec9245c4329a9745d0375aefe6e8c510cba
Terry <think@terry>
2012-12-15 21:10:00 -0700

    For corporate ownership controller, fixed JSON file to hold multiple companies, imported the file into the controller, and wrote a JSON->HTML parser in the view (which creates ul and li tags to represent the entire JSON structure recursively).

--- ruby/app/controllers/owner_controller.rb
+++ ruby/app/controllers/owner_controller.rb
@@ -1,4 +1,8 @@
+# Modified from: http://stackoverflow.com/a/7964378/770170
+require 'json'
+
 class OwnerController < ApplicationController
 	def index
+		@tree = JSON.parse File.read './db/owner/corporate-ownership-trees.json'
 	end
 end
