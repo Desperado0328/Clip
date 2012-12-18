84d2a242c4f8337678e341673f99a3ba8cd6da8d
Terry <think@terry>
2012-12-17 21:59:34 -0700

    Added reference to Stack Overflow question that made the stopwatch creation work.

--- ruby/app/models/stopwatch.rb
+++ ruby/app/models/stopwatch.rb
@@ -2,7 +2,7 @@ class Stopwatch < ActiveRecord::Base
   attr_accessible :time, :paused
   
   validates :time, :numericality => true
-  validates :paused, :inclusion => { :in => [true, false] }
+  validates :paused, :inclusion => { :in => [true, false] } # Don't test for presence of potentially-false values: http://stackoverflow.com/a/5219435/770170
   
   has_many :laps, :dependent => :destroy
 end
