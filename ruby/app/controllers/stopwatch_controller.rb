14245608cdc1fa916d83a252368b743ec2475639
Terry <think@terry>
2013-01-04 15:01:49 -0700

    Added (untested) pause and unpause (resume) methods, plus accessors for the current values of the stopwatch.

--- ruby/app/controllers/stopwatch_controller.rb
+++ ruby/app/controllers/stopwatch_controller.rb
@@ -1,4 +1,11 @@
 class StopwatchController < ApplicationController
+	before_filter :init_state_change, :only => [:destroy, :get_time, :get_lap_time, :pause, :resume, :lap]
+	
+	def init_state_change
+		@stopwatch = Stopwatch.find(params[:id])
+		@now = Time.now # Please don't inline this, to ensure consistent values
+	end
+	
 	def index
 		@stopwatches = Stopwatch.all
 	end
@@ -6,14 +13,17 @@ class StopwatchController < ApplicationController
 	# iPhone stopwatch operation (but don't follow the article's advice):
 	# http://www.leancrew.com/all-this/2009/07/iphone-stopwatch-ui-oddity/
 	
+	# Database transaction code modified from:
+	# http://api.rubyonrails.org/classes/ActiveRecord/Transactions/ClassMethods.html
+	
 	def create
-		@stopwatch = Stopwatch.new({
+		@stopwatch = Stopwatch.new(
 			:is_paused => true,
 			:total_at_last_pause => 0,
 			:lap_total_at_last_pause => 0,
 			:datetime_at_last_resume => nil,
 			:lap_datetime_at_last_resume => nil
-		})
+		)
 		if @stopwatch.save
 			flash[:notice] = 'Stopwatch was successfully created.'
 		else
@@ -23,12 +33,67 @@ class StopwatchController < ApplicationController
 	end
 	
 	def destroy
-		@stopwatch = Stopwatch.find(params[:id])
 		if @stopwatch.destroy
 			flash[:notice] = 'Stopwatch was successfully deleted.'
 		else
 			flash[:error] = ['Could not delete stopwatch because: ', @stopwatch.errors]
 		end
 		redirect_to stopwatch_path
-  end
+	end
+	
+	def get_time
+		ActiveRecord::Base.transaction do
+			if @stopwatch.is_paused
+				return @stopwatch.total_at_last_pause
+			else
+				return @stopwatch.total_at_last_pause + (@now - @stopwatch.datetime_at_last_resume)
+			end
+		end
+	end
+	
+	def get_lap_time
+		ActiveRecord::Base.transaction do
+			if @stopwatch.is_paused
+				return @stopwatch.lap_total_at_last_pause
+			else
+				return @stopwatch.lap_total_at_last_pause + (@now - @stopwatch.lap_datetime_at_last_resume)
+			end
+		end
+	end
+	
+	def pause
+		ActiveRecord::Base.transaction do
+			# TODO Assuming Time.now - Time.past is in milliseconds
+			milliseconds = @stopwatch.total_at_last_pause + (@now - @stopwatch.datetime_at_last_resume)
+			lap_milliseconds = @stopwatch.lap_total_at_last_pause + (@now - @stopwatch.lap_datetime_at_last_resume)
+			
+			@stopwatch.update_attributes(
+				:total_at_last_pause => milliseconds,
+				:lap_total_at_last_pause => lap_milliseconds,
+				:is_paused => true
+			)
+		end
+	end
+	
+	def resume
+		ActiveRecord::Base.transaction do
+			@stopwatch.update_attributes(
+				:datetime_at_last_resume => @now,
+				:lap_datetime_at_last_resume => @now,
+				:is_paused => false
+			)
+		end
+	end
+	
+	def lap
+		ActiveRecord::Base.transaction do
+			# TODO Assuming Time.now - Time.past is in milliseconds
+			lap_milliseconds = @stopwatch.lap_total_at_last_pause + (@now - @stopwatch.lap_datetime_at_last_resume)
+			Lap.create( :total => lap_milliseconds )
+			@stopwatch.update_attributes(
+				:lap_total_at_last_pause => 0,
+				:lap_datetime_at_last_resume => @now
+			)
+		end
+	end
 end
